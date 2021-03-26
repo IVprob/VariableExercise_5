bbd09d483649e42f18bba45a899086c356137ce2
IVProb <jnoojinjr@gmail.com>
2021-03-25 23:24:30 -0500

    initial commit

---
+++ VariableExercise_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariableExercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Doug";
+            int dogAge = 4;
+            char startWith = 'D';
+            bool haveDog = true;
+            double dogYears = 28.5;
+            decimal dogWeight = 53.24m;
+
+            Console.WriteLine($"My dog's name is {dogName} and he is {dogAge} in human years, but is {dogYears} in dog years. " +
+                $"He is a hefty boy weighing in at {dogWeight}lbs! His name starts with the letter: {startWith} and it is {haveDog} " +
+                $"he poops in the house.");
+        }
+    }
+}
